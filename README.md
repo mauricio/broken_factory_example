c76bf4a6050f5f2a59dc0741807ed948b8f44bb3
Mauricio Linhares <mauricio.linhares@gmail.com>
2014-07-27 16:01:47 -0300

    Include link at readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 broken_factory_example
 ======================
 
-Example project for a blog post.
+Example project for [a blog post](http://mauricio.github.io/2014/07/27/mutability-strikes-again.html).
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
 
   factory :user do
-    first_name "John"
-    last_name  "Doe"
-    options hometown: "Tokyo"
+    first_name { "John" }
+    last_name  { "Doe" }
+    options do
+      { hometown: "Tokyo" }
+    end
   end
 
 end
